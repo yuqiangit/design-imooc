ebeb1b5669e3a97db0aa4993eb21c489fe7f3330
shihuipeng <778184959@qq.com>
2016-07-23 18:16:37 +0800

    magic function

--- IMooc/Object.php
+++ IMooc/Object.php
@@ -10,7 +10,23 @@ namespace IMooc;
 
 class Object
 {
-    public static function hello(){
-        echo 'hello';
+    private $array = [];
+
+    public static function hello($name) {
+        echo 'hello-' . $name;
+    }
+
+    public function __set($name, $value) {
+        echo __METHOD__ . '<br>';
+        $this->array[$name] = $value;
+    }
+
+    public function __get($name) {
+        echo __METHOD__ . '<br>';
+        return $this->array[$name];
+    }
+
+    public function __call($name, $arguments) {
+        echo __METHOD__ . '<br>';
     }
 }
--- index.php
+++ index.php
@@ -9,5 +9,15 @@
 define('BASEDIR',__DIR__);
 include BASEDIR.'/IMooc/Loader.php';
 spl_autoload_register('\\IMooc\\Loader::autoload');
-\IMooc\Object::hello();
-\App\Controller\Home\Index::test();
+//\IMooc\Object::hello();
+//\App\Controller\Home\Index::test();
+
+$db = new \IMooc\DB();
+//$db->where('id = 1')->where('name= 1')->limit(10);
+
+/** @var object $obj */
+$obj = new \IMooc\Object();
+$obj->name = 'get';
+echo $obj->name.'<br>';
+
+echo $obj->test('hello','psh');
